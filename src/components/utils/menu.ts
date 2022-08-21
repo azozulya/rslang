a2211f471cd4f11d0f0e972885911ecae0752792
azozulya <bbbom@mail.ru>
2022-08-21 18:44:22 +0300

    feat: add burger menu, adaptive styles

--- src/components/utils/menu.ts
+++ src/components/utils/menu.ts
@@ -1,19 +1,48 @@
-const BurgerMenu = {
-  burgerMenu: document.getElementById('burgerMenu'),
+class Menu {
+  private burgerMenuBtn: HTMLElement | undefined | null;
 
-  init: () => {
-    BurgerMenu.burgerMenu?.addEventListener(
-      'click',
-      BurgerMenu.BurgerMenuHandler,
-    );
-  },
+  private menu: HTMLElement | null | undefined;
 
-  BurgerMenuHandler: () => {
-    BurgerMenu.burgerMenu?.classList.toggle('burger-menu__active');
-    //  menu.classList.toggle('menu--active');
+  private currentMenuItemClass = 'menu__link--active';
+
+  init() {
+    this.menu = document.getElementById('menu');
+    this.menu?.addEventListener('click', this.menuOverlayClickHandler);
+    this.burgerMenuBtn = document.getElementById('burgerMenu');
+    this.burgerMenuBtn?.addEventListener('click', this.burgerMenuHandler);
+  }
+
+  setActive = (pageName: string) => {
+    this.menu
+      ?.querySelector(`.${this.currentMenuItemClass}`)
+      ?.classList.remove(this.currentMenuItemClass);
+
+    this.menu
+      ?.querySelector(`[data-page=${pageName}]`)
+      ?.classList.add(this.currentMenuItemClass);
+  };
+
+  hide = () => {
+    if (!document.body.classList.contains('noscroll')) return;
+
+    this.burgerMenuBtn?.classList.remove('burger-menu__active');
+    this.menu?.classList.remove('menu--active');
+    document.body.classList.remove('noscroll');
+  };
+
+  private menuOverlayClickHandler = (event: Event) => {
+    const el = (<HTMLElement>event.target)?.closest('.menu__list');
+
+    if (el) return;
+
+    this.hide();
+  };
+
+  private burgerMenuHandler = () => {
+    this.burgerMenuBtn?.classList.toggle('burger-menu__active');
+    this.menu?.classList.toggle('menu--active');
     document.body.classList.toggle('noscroll');
-    // nav.classList.toggle('nav--active');
-  },
-};
+  };
+}
 
-export default BurgerMenu;
+export default Menu;
