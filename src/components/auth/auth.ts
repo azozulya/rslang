17006d539f5585481a0b1b5986c1775cda58b8dc
AdvisorGML <advisor@tut.by>
2022-08-24 23:45:44 +0300

    refactor: change validate and styling forms

--- src/components/auth/auth.ts
+++ src/components/auth/auth.ts
@@ -36,24 +36,30 @@ export default class Auth {
       // console.log(storage.refreshToken);
       if (storage.refreshToken === '') return false;
       const refreshTokenDate = (<IJwt>JWT(storage.refreshToken)).exp;
+      console.log(Math.trunc(Date.now() / 1000));
+      console.log(refreshTokenDate);
+
       const currentDate = Date.now() / 1000;
+      const differenceToken = refreshTokenDate - currentDate;
       // console.log(refreshTokenDate);
       // console.log(`${refreshTokenDate - currentDate / 1000}`);
-      if (refreshTokenDate - currentDate <= 3600) await this.user.getUserToken();
-      // if (refreshTokenDate - currentDate <= 3600) await this.api.getUserToken(storage.userId, storage.refreshToken);
-      return true;
+      if (differenceToken <= 3600 && differenceToken > 0) {
+        await this.user.getUserToken();
+        // if (refreshTokenDate - currentDate <= 3600) await this.api.getUserToken(storage.userId, storage.refreshToken);
+        return true;
+      }
     }
-    console.log('RSLang_Auth: NULL');
+    // console.log('RSLang_Auth: NULL');
     return false;
   }
 
   async drawButton() {
     const button = <HTMLElement>document.getElementById('auth');
     if (await this.isLogin()) {
-      button.innerHTML = '<button id="main_logout" class="btn btn--orange auth__btn">Выход</button>';
+      button.innerHTML = '<button id="main_logout" class="btn btn--orange auth__btn">Выйти</button>';
       button.addEventListener('click', (e: Event) => this.request(e));
     } else {
-      button.innerHTML = '<button id="main_login" class="btn btn--orange auth__btn">Вход</button>';
+      button.innerHTML = '<button id="main_login" class="btn btn--orange auth__btn">Войти</button>';
       button.addEventListener('click', (e: Event) => this.request(e));
     }
   }
@@ -62,6 +68,10 @@ export default class Auth {
     const container = <HTMLElement>document.createElement('div');
     container.id = this.container__class;
     document.body.append(container);
+    const loginWindow = <HTMLElement>document.getElementById(this.container__class);
+    loginWindow.addEventListener('click', (event) => {
+      if (event.target === loginWindow) this.closeModal();
+    });
     container.innerHTML = '<div id="modal"><div>';
 
     this.drawLoginUser();
@@ -76,71 +86,81 @@ export default class Auth {
         <div class="label">Имя</div>
         <div class="input"><input id="name" type="text" autocomplete="off" required></div>
       </div>
-      <div class="error">error</div>
+      <div id="error-name" class="error">&nbsp</div>
     </div>
     <div>
       <div class="email" data-validate="true">
         <div class="label">Email</div>
         <div class="input"><input id="email" type="email" autocomplete="off" required></div>
       </div>
-      <div id="error-email" class="error">error</div>
+      <div id="error-email" class="error">&nbsp;</div>
     </div>
     <div>
       <div class="password" data-validate="true">
         <div class="label">Пароль</div>
         <div class="input"><input id="password" type="password" autocomplete="off" minlength="8" required ></div>
       </div>
-      <div id="error-password" class="error">error</div>
+      <div id="error-password" class="error">&nbsp;</div>
     </div>
     <div class="button">
       <button id="close">ОТМЕНА</button><button id="register">РЕГИСТРАЦИЯ</button>
     </div>
   `;
     container.innerHTML = this.content;
-    const inputEmail = <HTMLInputElement>document.getElementById('email');
-    const errorEmail = <HTMLElement>document.getElementById('error-email');
-    // inputEmail.addEventListener('input', this.onInputEmail(inputEmail.value));
-    inputEmail.addEventListener('input', () => {
-      if (this.validateEmail(inputEmail.value)) errorEmail.innerText = 'Неверный формат Email-адреса';
-      else errorEmail.innerText = '';
-    });
 
+    this.addValidateEmail();
+    this.addValidatePassword();
     this.addHandlers();
   }
-
-  // private onInputEmail(value) {
-  //   const errorEmail = <HTMLElement>document.getElementById('error-email');
-  //   if (this.validateEmail(value)) {
-  //     errorEmail.innerText = 'green';
-  //   } else {
-  //     errorEmail.innerText = 'red';
-  //   }
-  // }
 
   drawLoginUser() {
     const container = <HTMLElement>document.getElementById('modal');
     container.innerHTML = '';
     this.content = `
     <div>
-      <div class="email" data-validate="true">
+      <div id="input-email" class="email" data-validate="true">
         <div class="label">Email</div>
         <div class="input"><input id="email" type="email" autocomplete="off" required></div>
       </div>
-      <div id="error-email" class="error">error</div>
+      <div id="error-email" class="error">&nbsp;</div>
     </div>
     <div>
       <div class="password" data-validate="true">
         <div class="label">Пароль</div>
         <div class="input"><input id="password" type="password" autocomplete="off" minlength="8" required></div>
       </div>
-      <div id="error-password" class="error">error</div>
+      <div id="error-password" class="error">&nbsp;</div>
     </div>
       <div class="button">
         <button id="registerLink">Регистрация</button><button id="close">ОТМЕНА</button><button id="login">ВХОД</button>
       </div>
   `;
     container.innerHTML = this.content;
+    const inputEmail = <HTMLElement>document.getElementById('input-email');
+    inputEmail.addEventListener('click', () => {
+      (<HTMLInputElement>document.getElementById('email')).focus();
+    });
+    this.addValidateEmail();
+    this.addValidatePassword();
     this.addHandlers();
+  }
+
+  addValidateEmail() {
+    const inputEmail = <HTMLInputElement>document.getElementById('email');
+    const errorEmail = <HTMLElement>document.getElementById('error-email');
+    inputEmail.addEventListener('input', () => {
+      if (!this.validateEmail(inputEmail.value)) errorEmail.innerText = 'Неверный формат Email-адреса';
+      else errorEmail.innerHTML = '&nbsp;';
+    });
+  }
+
+  addValidatePassword() {
+    const inputPassword = <HTMLInputElement>document.getElementById('password');
+    const errorPassword = <HTMLElement>document.getElementById('error-password');
+    inputPassword.addEventListener('input', () => {
+      if (inputPassword.value.length < 8) errorPassword.innerText = 'Длинна пароля не менее 8 символов';
+      else errorPassword.innerHTML = '&nbsp;';
+    });
   }
 
   private addHandlers() {
@@ -150,11 +170,14 @@ export default class Auth {
 
   private request(event: Event) {
     const target = <HTMLElement>event.target;
+    console.log(target);
+
     if (target.id === 'main_login') this.draw();
     if (target.id === 'registerLink') this.drawCreateUser();
     if (target.id === 'login') this.sendLoginData();
     if (target.id === 'register') this.sendRegisterData();
     if (target.id === 'close') this.closeModal();
+    // if (target.id === 'input-email') (<HTMLInputElement>document.getElementById('email')).focus();
   }
 
   private closeModal() {
@@ -167,7 +190,11 @@ export default class Auth {
     const password = (<HTMLInputElement>document.getElementById('password')).value;
     const res = await this.user.loginUser({ email, password });
     console.log(`sendLoginData: ${res}`);
-    if (res === 200) (<HTMLElement>document.getElementById(this.container__class)).remove();
+    if (res === 200) {
+      this.closeModal();
+      const button = <HTMLElement>document.getElementById('auth');
+      button.innerHTML = '<button id="main_logout" class="btn btn--orange auth__btn">Выйти</button>';
+    }
   }
 
   private async sendRegisterData() {
@@ -175,12 +202,16 @@ export default class Auth {
     const email = (<HTMLInputElement>document.getElementById('email')).value;
     const password = (<HTMLInputElement>document.getElementById('password')).value;
     const statusCreate = await this.user.createUser({ name, email, password });
-    console.log(statusCreate);
     if (statusCreate === 200) {
       console.log('REGISTER: OK');
       const statusLogin = await this.user.loginUser({ email, password });
-      (<HTMLElement>document.getElementById(this.container__class)).remove();
-      if (statusLogin === 200) console.log('LOGIN: OK');
+      if (statusLogin === 200) {
+        console.log('LOGIN: OK');
+        this.closeModal();
+        this.closeModal();
+        const button = <HTMLElement>document.getElementById('auth');
+        button.innerHTML = '<button id="main_logout" class="btn btn--orange auth__btn">Выйти</button>';
+      }
     }
     if (statusCreate === 417) {
       (<HTMLInputElement>document.getElementById('name')).value = '';
@@ -191,7 +222,6 @@ export default class Auth {
   }
 
   private validateEmail(value: string): boolean {
-    // const EMAIL_REGEXP = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
     const EMAIL_REGEXP = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return EMAIL_REGEXP.test(value);
   }
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
 import App from './components/App';
 import './assets/scss/index.scss';
-import createStorage from './components/utils/initStorage';
-
-createStorage();
 
 const app = new App();
 app.start();
